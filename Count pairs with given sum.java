6a001819b2840ee05fce68824e4b9370f4098700
Darshil Thummar <thummardarshil9687@gmail.com>
2023-11-06 11:08:15 +0530

    Create Count pairs with given sum.java

---
+++ Count pairs with given sum.java
@@ -0,0 +1,14 @@
+ HashMap<Integer, Integer> m = new HashMap<>();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (m.containsKey(k - arr[i])) {
+                count += m.get(k - arr[i]);
+            }
+            if (m.containsKey(arr[i])) {
+                m.put(arr[i], m.get(arr[i]) + 1);
+            }
+            else {
+                m.put(arr[i], 1);
+            }
+        }
+        return count;
