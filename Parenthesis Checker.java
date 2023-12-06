c43ef4f7770432aba357dd360b3dd23cffdb51df
Darshil Thummar <thummardarshil9687@gmail.com>
2023-12-06 22:33:08 +0530

    Create Parenthesis Checker.java

---
+++ Parenthesis Checker.java
@@ -0,0 +1,23 @@
+class Solution
+{
+    //Function to check if brackets are balanced or not.
+    static boolean ispar(String s)
+    {
+        int i = -1;
+        char[] stack = new char[s.length()];
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[')
+                stack[++i] = ch;
+            else {
+                if (i >= 0
+                    && ((stack[i] == '(' && ch == ')')
+                        || (stack[i] == '{' && ch == '}')
+                        || (stack[i] == '[' && ch == ']')))
+                    i--;
+                else
+                    return false;
+            }
+        }
+        return i == -1;
+    }
+}
