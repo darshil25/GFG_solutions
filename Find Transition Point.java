a8c897c5c4e22368a39c207f584c21da24cace6e
Darshil Thummar <thummardarshil9687@gmail.com>
2023-12-15 22:03:31 +0530

    Create Find Transition Point.java

---
+++ Find Transition Point.java
@@ -0,0 +1,20 @@
+class Solution {
+    int transitionPoint(int arr[], int n) {
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == 1 && (mid == 0 || arr[mid - 1] == 0)) {
+                return mid;
+            } else if (arr[mid] == 1) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
