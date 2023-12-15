515809eb5bb9412870e74f51b27100b64d6fec64
Darshil Thummar <thummardarshil9687@gmail.com>
2023-12-15 22:05:03 +0530

    Create Height of Binary Tree.java

---
+++ Height of Binary Tree.java
@@ -0,0 +1,15 @@
+class Solution {
+    //Function to find the height of a binary tree.
+    int height(Node node) 
+    {
+        if (node == null) {
+            return 0;
+        } else {
+    
+            int leftHeight = height(node.left);
+            int rightHeight = height(node.right);
+
+            return 1 + Math.max(leftHeight, rightHeight);
+        }
+    }
+}
