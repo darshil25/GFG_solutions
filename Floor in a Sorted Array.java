405f4bb0a9532148e06594c3ff1a32d00e5b6b92
Darshil Thummar <thummardarshil9687@gmail.com>
2023-12-21 21:54:48 +0530

    Create Floor in a Sorted Array.java

---
+++ Floor in a Sorted Array.java
@@ -0,0 +1,27 @@
+class Solution{
+    
+    // Function to find floor of x
+    // arr: input array
+    // n is the size of array
+    static int findFloor(long arr[], int n, long x)
+    {
+        int low = 0, high = n - 1;
+        int result = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] < x) {
+                result = mid;  
+                low = mid + 1;
+            } else {
+                high = mid - 1; 
+            }
+        }
+
+        return result;
+    }
+    
+}
