40cd6f0b2eb989b650938205412d8df6eba44414
Darshil Thummar <thummardarshil9687@gmail.com>
2023-12-21 21:51:27 +0530

    Create Candy.java

---
+++ Candy.java
@@ -0,0 +1,25 @@
+class Solution {
+    static int minCandy(int n, int ratings[]) {
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int totalCandies = 0;
+        for (int i = 0; i < n; i++) {
+            totalCandies += candies[i];
+        }
+
+        return totalCandies;
+    }
+}
