27a791cbb1adec6e84ab925a676384761022b57a
Darshil Thummar <thummardarshil9687@gmail.com>
2023-12-27 15:09:37 +0530

    Create Anti Diagonal Traversal of Matrix.java

---
+++ Anti Diagonal Traversal of Matrix.java
@@ -0,0 +1,32 @@
+class Solution
+{
+    public int[] antiDiagonalPattern(int[][] matrix)
+    {
+        int n = matrix.length;
+        int[] result = new int[n * n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            int row = 0;
+            int col = i;
+            while (row < n && col >= 0) {
+                result[index++] = matrix[row][col];
+                row++;
+                col--;
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            int row = i;
+            int col = n - 1;
+            while (row < n && col >= 0) {
+                result[index++] = matrix[row][col];
+                row++;
+                col--;
+            }
+        }
+
+        return result;
+
+    }
+}
