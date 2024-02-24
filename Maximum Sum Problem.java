81ce4e726d5aab5d4768372a6860b99cef91714a
Darshil Thummar <thummardarshil9687@gmail.com>
2024-02-24 22:55:23 +0530

    Create Maximum Sum Problem.java

---
+++ Maximum Sum Problem.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maxSum(int n) {
+        // code here.
+        if (n == 0 || n == 1)
+            return n;
+
+        return Math.max((maxSum(n / 2) + maxSum(n / 3) + maxSum(n / 4)), n);
+    }
+}
